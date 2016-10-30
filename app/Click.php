72bff02da5a87a205ec215e10fbcd4440af14295
AdrianM <adrianmoron@o2.pl>
2016-10-30 20:28:06 +0100

    Added relations to Users, Links and Clicks.

--- app/Click.php
+++ app/Click.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Click extends Model
 {
-    //
+    /**
+     * Get the link that owns the clicks.
+     */
+    public function link()
+    {
+        return $this->belongsTo('App\Link');
+    }
 }
--- app/Link.php
+++ app/Link.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+	/**
+     * Get the user that owns the link.
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get the link's clicks.
+     */
+    public function clicks()
+    {
+        return $this->hasMany('App\Click');
+    }
 }
